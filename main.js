a46d0ef967ad5be3f5d74fd2ce277c73adeca034
cbowman422 <chrisb0wman@hotmail.com>
2022-11-16 13:32:03 -0500

    added a war execution function

--- main.js
+++ main.js
@@ -109,7 +109,6 @@ function buttonDrawClick() {
 
 //The metacharacter \d search for digits, which are also numbers. The match() method uses regular expressions to retrieve it results. When used the match() with \d, it returns the number
 
-
 function checkWinnerHand(){
     if (playerOneCurrentHandNumber > playerTwoCurrentHandNumber){
         console.log(" player 1s hand")
@@ -117,7 +116,7 @@ function checkWinnerHand(){
         playerOneDeck.push(playerTwoDeck[0])
         playerOneDeck.splice(0,1)
         playerTwoDeck.splice(0,1)
-        return 
+        
     }
     if (playerOneCurrentHandNumber < playerTwoCurrentHandNumber){
         console.log(" player 2s hand")
@@ -125,11 +124,83 @@ function checkWinnerHand(){
         playerTwoDeck.push(playerTwoDeck[0])
         playerOneDeck.splice(0,1)
         playerTwoDeck.splice(0,1)
-        return 
+        
     }
     if (playerOneCurrentHandNumber === playerTwoCurrentHandNumber){
         console.log("War")
+        warExecution()
     }
     console.log(playerOneDeck)
     console.log(playerTwoDeck)
+}
+
+function warExecution(){
+    if (playerOneDeck[4][0] === "1" || playerOneDeck[4][0] === "2" || playerOneDeck[4][0] === "3" || playerOneDeck[4][0] === "4" || playerOneDeck[4][0] === "5" || playerOneDeck[4][0] === "6" || playerOneDeck[4][0] === "7" || playerOneDeck[4][0] === "8" || playerOneDeck[4][0] === "9"){
+        playerOneCurrentHand = playerOneDeck[4].match(/\d+/)
+        playerOneCurrentHandNumber = Number(playerOneCurrentHand)
+        console.log(playerOneCurrentHandNumber)
+    }
+    if(playerOneDeck[4][0] === "J"){
+        playerOneCurrentHandNumber = 11;
+        console.log(playerOneCurrentHandNumber)
+    }
+    if(playerOneDeck[4][0] === "Q"){
+        playerOneCurrentHandNumber = 12;
+        console.log(playerOneCurrentHandNumber)
+    }
+    if(playerOneDeck[4][0] === "K"){
+        playerOneCurrentHandNumber = 13;
+        console.log(playerOneCurrentHandNumber)
+    }
+    if(playerOneDeck[4][0] === "A"){
+        playerOneCurrentHandNumber = 14;
+        console.log(playerOneCurrentHandNumber)
+    }
+    // player 2
+    if (playerTwoDeck[4][0] === "1" || playerTwoDeck[4][0] === "2" || playerTwoDeck[4][0] === "3" || playerTwoDeck[4][0] === "4" || playerTwoDeck[4][0] === "5" || playerTwoDeck[4][0] === "6" || playerTwoDeck[4][0] === "7" || playerTwoDeck[4][0] === "8" || playerTwoDeck[4][0] === "9"){
+        playerTwoCurrentHand = playerTwoDeck[4].match(/\d+/)
+        playerTwoCurrentHandNumber = Number(playerTwoCurrentHand)
+        console.log(playerTwoCurrentHandNumber)
+    }
+    if(playerTwoDeck[4][0] === "J"){
+        playerTwoCurrentHandNumber = 11;
+        console.log(playerTwoCurrentHandNumber)
+    }
+    if(playerTwoDeck[4][0] === "Q"){
+        playerTwoCurrentHandNumber = 12;
+        console.log(playerTwoCurrentHandNumber)
+    }
+    if(playerTwoDeck[4][0] === "K"){
+        playerTwoCurrentHandNumber = 13;
+        console.log(playerTwoCurrentHandNumber)
+    }
+    if(playerTwoDeck[4][0] === "A"){
+        playerTwoCurrentHandNumber = 14;
+        console.log(playerTwoCurrentHandNumber)
+    }
+    checkWinnerHandWar()
+}
+
+function checkWinnerHandWar(){
+    if (playerOneCurrentHandNumber > playerTwoCurrentHandNumber){
+        console.log(" player 1s hand war")
+        playerOneDeck.push(playerOneDeck[0], playerOneDeck[1], playerOneDeck[2], playerOneDeck[3], playerOneDeck[4])
+        playerOneDeck.push(playerTwoDeck[0], playerTwoDeck[1], playerTwoDeck[2], playerTwoDeck[3], playerTwoDeck[4])
+        playerOneDeck.splice(0,5)
+        playerTwoDeck.splice(0,5)
+        
+    }
+    if (playerOneCurrentHandNumber < playerTwoCurrentHandNumber){
+        console.log(" player 2s hand war")
+        playerTwoDeck.push(playerOneDeck[0], playerOneDeck[1], playerOneDeck[2], playerOneDeck[3])
+        playerTwoDeck.push(playerTwoDeck[0], playerTwoDeck[1], playerTwoDeck[2], playerTwoDeck[3])
+        playerOneDeck.splice(0,5)
+        playerTwoDeck.splice(0,5)
+        
+    }
+    if (playerOneCurrentHandNumber === playerTwoCurrentHandNumber){
+        console.log("War 2")
+        warExecution()
+    }
+ 
 }
